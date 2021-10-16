97e22f575a088f2008dec62fa18a8638bb7ae514
Amitt Dosanjh <amitt.dosanjh@gmail.com>
2021-10-15 23:59:04 -0500

    Formatting

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -4,10 +4,11 @@ const parseCookies = (req, res, next) => {
   if (!req.headers.cookie) {
     return next();
   }
-
   req.cookies = Object.create(null);
+
   var parse = req.headers.cookie;
   parse = parse.split(';').join().split(', ');
+
   parse.forEach((item) => {
     item = item.split('=');
     req.cookies[item[0]] = item[1];
